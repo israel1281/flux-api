5772afe22180831d2ada368343b9cfe683c044ce
Israel David <77063876+israel1281@users.noreply.github.com>
2022-05-02 02:45:00 -0700

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose')
 
 
 const userSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 25
+    },
     email: {
         type: String,
         required: true,
@@ -12,6 +18,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    mobile: {type: String, default: ''},
+    gender: {type: String, default: 'male'},
+    referrer: [{type: mongoose.Types.ObjectId, ref: 'user'}],
+    referring: [{type: mongoose.Types.ObjectId, ref: 'user'}],
 }, {
     timestamps: true
 })
